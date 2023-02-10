ae609c681cb8ce1fe9d0ca0cba8ed51182bb6796
lucas lamar <llamareo@gmail.com>
2023-02-10 15:42:09 -0600

    changed output so it goes to a .csv instead of a .txt

--- log_app.py
+++ log_app.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from zoneinfo import *
 import time
+import csv
 import os.path
 
 class Log():
@@ -44,50 +45,35 @@ class Log():
         if message != '':
             message = ' - {0}'.format(message)
             message_lower=message.lower()
-            if 'lucas' in message_lower:
+            if 'bird' in message_lower:
                 if os.path.isfile('./mylog.txt'):
                     mode = 'a'
                 else:
                     mode = 'w'
                 f = open('mylog.txt', mode, encoding='utf-8')
-                f.writelines('Hey aiden, if you\'re seeing this that means you typed my name as\n'
-                             + 'one of your entries, which I\'ll assume means you\'re thinking\n'
-                             + 'about me for one reason or another. I wanted to let you know\n'
-                             + 'that I really care about you and miss you so much and haven\'t\n'
-                             + 'really gone a day '
-                             + 'without thinking about you. I really hope you\'re doing well\n'
-                             + 'but then there\'s another selfish part of me that hopes you\n'
-                             + 'aren\'t just because I\'m not and I don\'t want you to be doing\n'
-                             + 'well without me. I know that\'s super selfish and dumb but it\'s true\n'
-                             + 'which is so annoying. I was really missing you a lot today and wanted\n'
-                             + 'to text you so bad and let you know that I was thinking about you\n'
-                             + 'but I didn\'t because I know that you need space to get over me\n'
-                             + 'so I\'m doing this instead. I miss you so much though. I miss\n'
-                             + 'your smile, I miss your laugh, I miss your annoying little bites, I\n'
-                             + 'miss how freaking pretty you are. I hate how much I like your new\n'
-                             + 'haircut because it is not making this easier for me lol but it\n'
-                             + 'does look really good on you. I hope everything is going well with\n'
-                             + 'school and the raptor center and all that. I also really hope you\n'
-                             + 'don\'t move on from me to fast as I will likely not move too soon myself.\n'
-                             + 'I know that\'s also selfish of me but it would really hurt me.\n'
-                             + 'I know Sean and Mel broke up so a piece of me is worried that you\n'
-                             + 'and Sean are gonna rebound with each other if you haven\'t already.\n'
-                             + 'Anyways, if you do end up seeing this, some reassurance about that\n'
-                             + 'would be really nice to hear from you. Anyways, just know I care\n'
-                             + 'about you and I love you. That\'s all I think.\n')
+                f.writelines('looks like you wrote the word \'bird\'! freaking bird person XD.')
                 f.close()
         return message
 
     def write_entry(self):
-        entry = (self.month_to_name(self.month) + ' ' + str(self.day)+ ', ' + str(self.year) 
-                 + " @" + str(self.hour) + ":" + str(self.minute) + self.message + '\n')
-        if os.path.isfile('./mylog.txt'):
+
+        
+        if not os.path.isfile('./mylog.csv'):
+            with open("mylog.csv", 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(["Month", "Day", "Year", "Hour", "Minute", "Memo"])
+
+
+
+
+        if os.path.isfile('./mylog.csv'):
             mode = 'a'
         else:
             mode = 'w'
-        f = open('mylog.txt', mode, encoding='utf-8')
-        f.writelines(entry)
-        f.close()
+        with open("mylog.csv", mode) as f:
+            writer = csv.writer(f)
+            writer.writerow([self.month_to_name(self.month), self.day, self.year, self.hour,
+                             self.minute, self.message])
 
 my_log = Log()
 my_log.write_entry()
